d2df491601da0b64fe17844fff657416de383932
Robin <rinborobin629@gmail.com>
2026-09-02 19:27:09 +0700

    feat(api): tasks basic crud operation

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -22,7 +22,72 @@ router.get("/", async (req, res) => {
                 ON t.course_id = c.course_id
         `);
 
-    res.json(rows);
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "Failed to fetch tasks",
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const [rows] = await pool.query(
+      `SELECT
+                t.task_id,
+                t.title,
+                t.description,
+                t.due_date,
+                t.priority,
+                s.name AS status,
+                c.title AS course,
+                c.code AS course_code
+            FROM tasks t
+            JOIN status s
+                ON t.status_id = s.status_id
+            JOIN courses c
+                ON t.course_id = c.course_id
+            WHERE task_id = ?`,
+      [id],
+    );
+    if (rows.length === 0) {
+      res.status(204).json(null);
+    }
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "Failed to fetch tasks",
+    });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { courseId, statusId, title, description, dueDate, priority } =
+      req.body;
+
+    const [result] = await pool.query(
+      `
+            UPDATE 
+                    tasks
+            SET
+                    course_id = ?,
+                    status_id = ?,
+                    title = ?,
+                    description = ?,
+                    due_date = ?,
+                    priority = ?
+            WHERE task_id = ?;`,
+      [courseId, statusId, title, description, dueDate, priority, id],
+    );
+
+    res.status(200).json({ message: "Task updated", taskId: result.taskId });
   } catch (error) {
     console.error(error);
 
@@ -34,10 +99,46 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    const { course_id, status_id, title, description, due_date, priority } =
+    const { courseId, statusId, title, description, dueDate, priority } =
       req.body;
 
-    res.send(req.body);
+    const [result] = await pool.query(
+      `INSERT INTO tasks
+             (course_id, status_id, title, description, due_date, priority)
+             VALUES (?, ?, ?, ?, ?, ?)`,
+      [courseId, statusId, title, description, dueDate, priority],
+    );
+
+    res.status(201).json({
+      message: "Task created",
+      taskId: result.insertId,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "Failed to fetch tasks",
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const [result] = await pool.query(`DELETE FROM tasks WHERE task_id = ?`, [
+      id,
+    ]);
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({
+        error: "Task not found",
+      });
+    }
+
+    res.json({
+      message: "Task deleted",
+    });
   } catch (error) {
     console.error(error);
 
